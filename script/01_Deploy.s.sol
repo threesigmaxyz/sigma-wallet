ad3a2a1f86f9a9e62962ccd7720d1305bdc4d21a
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-07-21 23:52:43 +0200

    chore:  clean + set-up foundry template

--- script/01_Deploy.s.sol
+++ script/01_Deploy.s.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.0;
 
 import { Script } from "@forge-std/Script.sol";
 
-import { Greeter } from "src/Greeter.sol";
-
 /// @dev See the "Solidity Scripting" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/tutorials/solidity-scripting?highlight=scripts#solidity-scripting
 contract Deploy is Script {
@@ -17,7 +15,7 @@ contract Deploy is Script {
         vm.startBroadcast();
 
         // deploy contract
-        new Greeter("GM");
+        // new Greeter("GM");
 
         vm.stopBroadcast();
     }
