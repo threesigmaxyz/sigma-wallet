74ee56aba10463b1e8886e93c87dc94b6989d6f4
Joao Lopes <joao.lopes@threesigma.xyz>
2023-07-22 23:05:04 +0100

    feat: add testnet scripts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.0;
 
 import { Script } from "@forge-std/Script.sol";
 
+import { GoogleProvider } from "src/providers/GoogleProvider.sol";
+import { ProviderManager } from "src/providers/ProviderManager.sol";
+
+
 /// @dev See the "Solidity Scripting" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/tutorials/solidity-scripting?highlight=scripts#solidity-scripting
 contract Deploy is Script {
+
+    address chainlinkOracle = 0x649a2C205BE7A3d5e99206CEEFF30c794f0E31EC;
+
     function setUp() public {
         // solhint-disable-previous-line no-empty-blocks
     }
@@ -14,8 +21,9 @@ contract Deploy is Script {
     function run() public {
         vm.startBroadcast();
 
-        // deploy contract
-        // new Greeter("GM");
+        ProviderManager providerManager = new ProviderManager();
+        GoogleProvider google = new GoogleProvider(chainlinkOracle);
+        providerManager.addProviderSimple(google, "Google");
 
         vm.stopBroadcast();
     }
---
+++ script/makeTransaction.s.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { Script } from "@forge-std/Script.sol";
+
+import { SigmaWallet } from "src/SigmaWallet.sol";
+import { SigmaWalletFactory } from "src/SigmaWalletFactory.sol";
+
+import { GoogleProvider } from "src/providers/GoogleProvider.sol";
+import { ProviderManager } from "src/providers/ProviderManager.sol";
+
+import { IEntryPoint } from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import { UserOperationLib, UserOperation } from "@account-abstraction/contracts/interfaces/UserOperation.sol";
+
+
+
+/// @dev See the "Solidity Scripting" section in the Foundry Book if this is your first time with Forge.
+/// https://book.getfoundry.sh/tutorials/solidity-scripting?highlight=scripts#solidity-scripting
+contract makeTransaction is Script {
+
+    IEntryPoint public constant ENTRY_POINT = IEntryPoint(0x0576a174D229E3cFA37253523E645A78A0C91B57);
+    address google = address(0); // NEEDS UPDATE
+    address providerManager = address(0); // NEEDS UPDATE
+    address ourAddr = 0xff07F25C4753BE90D919A908F54Eb64adA79DD3d;
+
+    function setUp() public {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /// @dev You can send multiple transactions inside a single script.
+    function run() public {
+        vm.startBroadcast();
+
+        SigmaWalletFactory factory = new SigmaWalletFactory(ENTRY_POINT, ProviderManager(providerManager));
+        SigmaWallet aliceWallet = SigmaWallet(payable(factory.getAddress("aliceGoogleId", 0)));
+
+        UserOperation[] memory userOperations_ = new UserOperation[](1);
+
+        userOperations_[0].sender = address(aliceWallet);
+        userOperations_[0].nonce = 0;
+        userOperations_[0].initCode =
+            abi.encodePacked(address(factory), abi.encodeCall(factory.createAccount, ("aliceGoogleId", 0)));
+        userOperations_[0].callData;
+        userOperations_[0].callGasLimit = 10e6;
+        userOperations_[0].verificationGasLimit = 10e6;
+        userOperations_[0].preVerificationGas = 0;
+        userOperations_[0].maxFeePerGas = 20e9;
+        userOperations_[0].maxPriorityFeePerGas = 2e9;
+        userOperations_[0].paymasterAndData;
+
+        string memory providerName_ = "Google";
+        string memory headerJson_ = '{"alg":"RS256","kid": "b2dff78a0bdd5a02212063499d77ef4deed1f65b", "typ":"JWT"}';
+        string memory payloadJson_ =
+            '{"sub":"aliceGoogleId","name":"John Doe","iat":1516239022,"nonce":"xf30B2uPOlNXxeOVq5cLW1QJj-8","aud":"theaudience.zeppelin.solutions"}';
+        bytes memory signature = "0x123456789abcd";
+        bytes memory data = abi.encode(providerName_, headerJson_, payloadJson_, signature);
+        userOperations_[0].signature = data; //_createSignature(abi.encode(providerName_, jwtToken_), ALICE_PK);
+        ENTRY_POINT.handleOps(userOperations_, payable(ourAddr));
+
+        vm.stopBroadcast();
+    }
+}
